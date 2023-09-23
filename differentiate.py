bdf3092efae7d1f3f6d4fc6b915a4e4b0b91f9f1
930r91na <georgina.zeronca@udlap.mx>
2023-09-23 11:15:59 -0600

    Updated project files from [person's name]

--- differentiate.py
+++
@@ -1,8 +0,0 @@
-import sympy as sp
-from sympy.utilities.lambdify import lambdify
-
-def differentiate(f):
-    x = sp.Symbol('x')
-    df = sp.diff(f, x)
-    df2 = sp.diff(df, x)
-    return lambdify(x, df, 'numpy'), lambdify(x, df2, 'numpy')
--- menu.py
+++ menu.py
@@ -1,79 +1,93 @@
+import MBisection as k
+import MNewton as n
+import MModified as m
+import MSecant as s
 import sys
 import sympy as sp
-from sympy import *
-
-import ErrorHandling as errorHandling
-import differentiate as diff
-import bisectionMethod as bM
-import newtonMethod as nM
-import newtonModifiedMethod as nMM
-import secantMethod as sM
-import printOutput as pO
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def askFunction():
-    print("\nIt is time to introduce your function f in terms of x:\n")
-
-    expression = input("\nInsert your function: ")
-    try:
-        f = sp.sympify(expression)
-        return f
-    except sp.SympifyError:
-        raise errorHandling.InvalidInput("Invalid input. Please enter a valid mathematical expression.")
+def display_menu():
+    """Display the available root-finding methods."""
+    print("\nSelect a method to find the root of the function:")
+    methods = ["Bisection", "Newton", "Newton's Modified Method", "Secant", "Exit"]
+    for index, method in enumerate(methods):
+        print(index + 1, method)
+    return int(input("\nSelect the method number: "))
 
 
-def menu():
-    ans = True
-    while ans:
-        print("-----Menu-----")
-        print("1) Bisection method \n2) Newton's method \n3) Newton's modified method \n4) Secant method \n5) Exit")
+def get_root(choice, f, df, ddf):
+    """Execute the selected root-finding method."""
+    if choice == 1:
+        a = float(input("Enter the starting interval a: "))
+        b = float(input("Enter the ending interval b: "))
+        tol = float(input("Enter the tolerance: "))
+        print("\nUsing Bisection method:")
+        return k.my_bisection(f, a, b, tol)
+    elif choice == 2:
+        x0 = float(input("Enter the initial guess x0: "))
+        epsilon = float(input("Enter the tolerance: "))
+        max_iter = int(input("Enter the maximum number of iterations: "))
+        print("\nUsing Newton method:")
+        return n.newton(f, df, x0, epsilon, max_iter)
+    elif choice == 3:
+        x0 = float(input("Enter the initial guess x0: "))
+        epsilon = float(input("Enter the tolerance: "))
+        max_iter = int(input("Enter the maximum number of iterations: "))
+        print("\nUsing Modified Newton-Raphson method:")
+        return m.modified(f, df, ddf, x0, epsilon, max_iter)
+    elif choice == 4:
+        x0 = float(input("Enter the first initial guess x0: "))
+        x1 = float(input("Enter the second initial guess x1: "))
+        epsilon = float(input("Enter the tolerance: "))
+        max_iter = int(input("Enter the maximum number of iterations: "))
+        print("\nUsing Secant method:")
+        return s.secant(f, x0, x1, epsilon, max_iter)
+    elif choice == 5:
+        print("Exiting the program. Goodbye!")
+        sys.exit()
+    else:
+        print("Invalid selection. Please try again.")
+        return None
 
-        # take input from user
-        ans = input("Choose an option from the Menu shown below: \n")
 
-        # type cast into integer
-        ans = int(ans)
+def plot_function(f, root=None, xmin=-10, xmax=10):
+    x_vals = np.linspace(xmin, xmax, 400)
+    y_vals = [f(x) for x in x_vals]
+    fig = plt.figure()
+    fig.suptitle('Graph of f(x)', fontsize=12)
+    plt.plot(x_vals, y_vals, 'b-', label='f(x)')
+    if root:
+        plt.scatter(root, f(root), color='red', label=f'Root: {root:.5f}')
+    plt.xlim(xmin, xmax)
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    return fig
 
-        if ans == 1:
-            print("Bisection Method\n")
-            f = askFunction()
-            a = int(input("Type the left end of the interval, value of a: "))
-            b = int(input("Type the right end of interval (a,b), in fact, give the value of b: "))
-            tol = float(input("Type the tolerance: "))
-            root = bM.bisectionMeth(lambdify('x', f), a, b, tol)
-            pO.print_output(lambdify('x', f), root, tol)
 
-        elif ans == 2:
-            print("Newton's Method\n")
-            f = askFunction()
-            f_prime = diff.differentiate(f)
-            x0 = float(input("Enter the initial guess x0: "))
-            tol = float(input("Enter the tolerance level: "))
-            max_iter = int(input("Enter the maximum number of iterations: "))
-            root = nM.newton(lambdify('x', f), f_prime, x0, tol, max_iter)
-            pO.print_output(lambdify('x', f), root, tol)
+def main():
+    """Main function to run the program."""
+    x = sp.symbols('x')
 
-        elif ans == 3:
-            print("Newton's Modified Method\n")
-            f = askFunction()
-            f_prime, f_biprime = diff.differentiate(f)
-            x0 = float(input("Enter the initial guess x0: "))
-            tol = float(input("Enter the tolerance level: "))
-            max_iter = int(input("Enter the maximum number of iterations: "))
-            root = nMM.newtonModified(lambdify('x', f), f_prime, f_biprime, x0, tol, max_iter)
-            pO.print_output(lambdify('x', f), root, tol)
+    while True:
+        user_function = input("\nEnter your function in terms of x (e.g., x**2 - 2): ")
+        f_expr = sp.sympify(user_function)
+        f = sp.lambdify(x, f_expr, "numpy")
+        df_expr = sp.diff(f_expr, x)
+        df = sp.lambdify(x, df_expr, "numpy")
+        ddf_expr = sp.diff(df_expr, x)
+        ddf = sp.lambdify(x, ddf_expr, "numpy")
 
-        elif ans == 4:
-            print("Secant Method\n")
-            f = askFunction()
-            x0 = float(input("Enter the initial guess x0: "))
-            x1 = float(input("Enter a second zero guess x1: "))
-            tol = float(input("Enter the tolerance level: "))
-            max_iter = int(input("Enter the maximum number of iterations: "))
-            root = sM.secantMethod(lambdify('x', f), x0, x1, tol, max_iter)
-            pO.print_output(lambdify('x', f), root, tol)
+        user_choice = display_menu()
+        root = get_root(user_choice, f, df, ddf)
+        if root is not None:
+            print(f"The root of the function {user_function} is approximately: {root:.5f}")
+            plot_function(f, root)
 
-        elif ans == 5:
-            sys.exit("Bye")
-        else:
-            print("Invalid choice. Please enter one of the options shown above")
+
+if __name__ == "__main__":
+    main()
